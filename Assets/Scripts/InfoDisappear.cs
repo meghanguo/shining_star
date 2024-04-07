e955b8e6df54a8b2b722b6284d8f7b2818bf399e
Meghan Guo <99140623+meghanguo@users.noreply.github.com>
2024-04-07 16:27:15 -0500

    final unity files

---
+++ Assets/Scripts/InfoDisappear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoDisappear : MonoBehaviour
+{
+    public Canvas canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
